18422b8d5816d9933f94e24e988a04a13588fe55
Trevor Covington <tcovington@fabpro1.com>
2018-08-13 22:55:41 -0500

    Updates to use new version of csvToJson - Updated package.json to use new version of csvtojson - Updated parse-pokemon-data.js to use the new csvtojson Promise/subscribe functionality - Also updated parse-pokemon-data.js to only create one json file instead of two - Refactored pokemon.js to look through json file for either id or name

---
+++ index.js
@@ -0,0 +1,4 @@
+const Pokedex = require('./lib/index');
+const pokedex = new Pokedex();
+
+console.log(pokedex.pokemon(7));
--- scripts/parse-pokemon-data.js
+++ scripts/parse-pokemon-data.js
@@ -1,54 +1,39 @@
-var fs = require('fs'),
-    rs = fs.createReadStream('./data/csv/pokemon.csv'),
-    Converter = require('csvtojson').Converter,
-    convertNames = new Converter(),
-    convertIds = new Converter();
+const fs = require('fs');
 
-var pokemonNames = {},
-    pokemonIds = {};
+const pathToCsv = './data/csv/pokemon.csv';
+const csv = require('csvtojson');
 
-convertNames.on('end_parsed', function () {
-  fs.writeFile('./lib/pokemon-by-name.json', JSON.stringify(pokemonNames));
-});
 
-convertNames.on('record_parsed', function (resultRow) {
-  var name = resultRow.identifier;
-  delete resultRow.identifier;
-  resultRow.name = name;
-  if (resultRow.species_id < 650) {
-    resultRow.sprites = {
-      normal: 'http://img.pokemondb.net/sprites/black-white/normal/' + name + '.png',
-      animated: 'http://img.pokemondb.net/sprites/black-white/anim/normal/' + name + '.gif'
-    };
-  } else {
-    resultRow.sprites = {
-      normal: 'http://img.pokemondb.net/sprites/x-y/normal/' + name + '.png'
-    };
-  }
-  pokemonNames[name] = resultRow;
-});
-
-convertIds.on('end_parsed', function () {
-  fs.writeFile('./lib/pokemon-by-id.json', JSON.stringify(pokemonIds));
-});
-
-convertIds.on('record_parsed', function (resultRow) {
-  var id = resultRow.id,
-      name = resultRow.identifier;
-  delete resultRow.identifier;
-  resultRow.name = name;
-  if (resultRow.species_id < 650) {
-    resultRow.sprites = {
-      normal: 'http://img.pokemondb.net/sprites/black-white/normal/' + name + '.png',
-      animated: 'http://img.pokemondb.net/sprites/black-white/anim/normal/' + name + '.gif'
-    };
-  } else {
-    resultRow.sprites = {
-      normal: 'http://img.pokemondb.net/sprites/x-y/normal/' + name + '.png'
-    };
-  }
-  pokemonIds[id] = resultRow;
-});
-
-rs.pipe(convertNames);
-rs.pipe(convertIds);
+csv().fromFile(pathToCsv).subscribe((resultRow) => {
+    var name = resultRow.identifier;
+    delete resultRow.identifier;
+    resultRow.name = name;
+    if (resultRow.species_id < 650) {
+        resultRow.sprites = {
+            normal: 'http://img.pokemondb.net/sprites/black-white/normal/' + name + '.png',
+            animated: 'http://img.pokemondb.net/sprites/black-white/anim/normal/' + name + '.gif'
+        };
+    } else {
+        resultRow.sprites = {
+            normal: 'http://img.pokemondb.net/sprites/x-y/normal/' + name + '.png'
+        };
+    }
+})
+.then(obj => {
+    console.log('Writing pokemon data...');
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./lib/pokemon.json', JSON.stringify(obj), (error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve('File created');
+            }
+        })
+    });
+})
+.then(result => {
+    return console.log(result);
+})
+.catch(error => {
+    return console.error(error);
+})
