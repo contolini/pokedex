375710d5d9eb7328028e5567201ccf7abe8b456c
Tommaso A <tommaso.amici@me.com>
2018-12-14 18:12:10 +0100

    fixed export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,24 +1,19 @@
-interface Pokemon {
-  id: number;
-  name: string;
-  height?: number;
-  weight?: number;
-  sprites?: {
-    animated?: string;
-    normal?: string;
-  };
-  species_id?: number;
-  base_experience?: number;
-  order?: number;
-  is_default?: number;
-}
-
-declare function getPokemon(id: string | number): Pokemon;
-
-declare namespace Pokedex {
-  class Pokedex {
-    pokemon(id: string | number): Pokemon;
-  }
-}
 
 export = Pokedex;
+
+declare namespace Pokedex {
+  export interface Pokemon {
+    id: number;
+    name: string;
+    height?: number;
+    weight?: number;
+    sprites?: {animated?: string; normal?: string;};
+    species_id?: number;
+    base_experience?: number;
+    order?: number;
+    is_default?: number;
+  }
+  class Pokedex {
+    pokemon(id: string|number): Pokemon;
+  }
+}
